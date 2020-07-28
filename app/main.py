5b80aff32c04132d11080cea52ab8d640080b09a
amos-baron <amosbar1000@gmail.com>
2020-07-28 11:04:09 +0300

    first commit in the new repo, everything isnt stable yet, checkout old repo for whole commit history

---
+++ app/main.py
@@ -0,0 +1,29 @@
+from flask import Flask,request
+import chaos_injector_slave
+import os
+
+injector_url = os.environ.get("INJECTOR_API", "http://chaos.injector.openshift:5002")
+db_url = os.environ.get("DB_API", "http://chaos.db.openshift:5001")
+server_port = int(os.environ.get("SERVER_PORT", 5002))
+
+
+app = Flask(__name__)
+injection_slave = chaos_injector_slave.InjectionSlave(db_url)
+
+@app.route('/inject_fault',methods=['GET'])
+def get_instructions():
+    return "send dns and fault name in json object"
+
+@app.route('/inject_fault',methods=['POST'])
+def inject_fault():
+    dns = request.json['dns']
+    fault = request.json['fault']
+    output = call_slave(dns,fault)
+    return output
+
+def call_slave(dns,fault):
+    output = injection_slave.initiate_fault(dns, fault)
+    return  output
+if __name__ == '__main__':
+    app.run(debug=True)
+    app.run(host='0.0.0.0', port= server_port)
